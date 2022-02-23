dbbcdabef6793fe22c80fc89f678584fbda77dc2
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2022-02-23 15:37:21 +0300

    now you can insert elements after an arbitrary index -inside the list and outside the list

--- scr/doubly-linked-list.c
+++ scr/doubly-linked-list.c
@@ -7,18 +7,27 @@
 int main () {
 	list *Tokens = newList (25);
 
-
 	printList (Tokens);
-	for (unsigned newEl = 0; newEl < 10; newEl++) {
+
+	// printList (Tokens);
+	for (unsigned newEl = 3; newEl < 8; newEl++) {
+		printf ("\n\nINSERTING AFTER: %d\n", newEl);
 		InsertAfter (newEl, Tokens, 7);
-		getchar ();
+		// getchar ();
 		printList (Tokens);
 	}
 
-	drowCell ();
+	printf ("\n\nINSERTING AFTER: %d\n", 3);
+	InsertAfter (2, Tokens, 666);
+	printList (Tokens);
+	InsertAfter (2, Tokens, 666);
+	printList (Tokens);
+	InsertAfter (2, Tokens, 666);
+	printList (Tokens);
+	InsertAfter (2, Tokens, 666);
 	printList (Tokens);
 
-
+	drowCell ();
 	return 0;
 }
 
@@ -38,38 +47,59 @@ static inline unsigned listPrev (list *list, unsigned index) {
 	return list->cells[index].prev;
 }
 
+
+int FindEmpty (list *list) {
+	assert (list != nullptr);
+
+	// #change capacity to find new empty cells
+	if (list->cells[list->free].next == -2) {
+		ChangeCapacity (list, list->capacity * INCREASE_COEF);
+	}
+
+	int save_free = list->free;
+	list->free = list->cells[list->free].next;
+	return save_free;
+}
+
+
 // #want: to make function ...{After/before} as function-toggle
 void InsertAfter (int index, list *list, int value) {
 	assert (list  != nullptr);
 
-	// #if the user enters the index of
-	// #a free cell, then the program will
-	// #insert the token at the first free
-	// #index after the tail
-	// if (list->cells[index].prev == -1) {
-	// 	index = free;
-	// }
-
 	int freeIndex = FindEmpty (list);
-
 	list->cells[freeIndex].data.num_d = value;
+		
+	if (list->cells[index].prev < 0) {							// if we insert by position of free 
+		printf ("index is out of range of list\n");
+		printf ("empty: %u, index: %u\n", list->free, index);
+		printf ("freeIndex: %u\n", freeIndex);
+		list->cells[freeIndex].next = list->free;
 
-	list->cells[freeIndex].prev = index;
-	list->cells[listNext (list, index)].prev = freeIndex;
+		if (freeIndex == list->tail) {
+			list->cells[freeIndex].next = -2;
+		}
 
-	list->cells[freeIndex].next = list->cells[index].next;
-	list->cells[index].next = freeIndex;
+		if (list->cells[freeIndex].prev > -2) {
+			list->cells[list->tail].next = freeIndex;
+			list->cells[freeIndex].prev = list->tail;
+			list->tail = freeIndex;
+			list->cells[freeIndex].next = -2;
+		}
+	}
+	else {														// insert after index in list
+		printf ("[IN] empty: %u, index: %u\n", list->free, index);
+		printf ("[IN] freeIndex: %u\n", freeIndex);
+
+		list->cells[freeIndex].data.num_d = value;
+
+		list->cells[freeIndex].prev = index;
+		list->cells[listNext (list, index)].prev = freeIndex;
+
+		list->cells[freeIndex].next = list->cells[index].next;
+		list->cells[index].next = freeIndex;
+	}
 
 	list->size++;
-
-	// if (list->cells[freeIndex].next == 0 && list->cells[freeIndex].prev != -1) {
-	// 	list->tail = freeIndex;
-	// }
-
-	// if (list->cells[freeIndex].prev == 0) {
-	// 	list->head = freeIndex;
-	// }
-
 	return;
 }
 
@@ -92,7 +122,7 @@ struct list *newList (unsigned capacity) {
 			List->cells[el].next = -2;
 		}
 		else if (el == 0) {
-			List->cells[el].next = 0;
+			List->cells[el].next = 1;
 		}
 		else {
 			List->cells[el].next = el + 1;
@@ -121,21 +151,6 @@ void ChangeCapacity (list *list, unsigned capacity) {
 }
 
 
-
-int FindEmpty (list *list) {
-	assert (list != nullptr);
-
-	// #change capacity to find new empty cells
-	if (list->cells[list->free].next == -2) {
-	ChangeCapacity (list, list->capacity * INCREASE_COEF);
-	}
-
-	int save_free = list->free;
-	list->free = list->cells[list->free].next;
-	return save_free;
-}
-
-
 void printList (list *list) {
 	assert (list != nullptr);
 
@@ -150,9 +165,9 @@ void printList (list *list) {
 		list->cells[el].next);
 
 	// #drow pointers {head, tail, free} 
-	if (el == list->head) {printf ("<head ");}
-	if (el == list->tail) {printf ("<tail ");}
-	if (el == list->free) {printf ("<free ");}
+	if (el == list->head) {printf (" <head");}
+	if (el == list->tail) {printf (" <tail");}
+	if (el == list->free) {printf (" <free");}
 	printf ("\n");
 	}
 	return;
@@ -160,7 +175,7 @@ void printList (list *list) {
 
 
 void drowCell () {
-	FILE *viz = fopen ("viz.dot", "w");
+	FILE *viz = fopen ("viz/viz.dot", "w");
 	fprintf (viz, "graph G {\n");
 	fprintf (viz, "size=\"8.2, 8.2\"\n");
 
@@ -170,7 +185,7 @@ void drowCell () {
 	fprintf (viz, "}\n");
 
 	fclose (viz);
-	system ("dot viz.dot -T png -o viz.png");
+	system ("dot viz/viz.dot -T png -o viz/viz.png");
 }
 
 
